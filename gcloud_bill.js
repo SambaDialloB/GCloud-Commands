7115514c36e255bb6b5a832c7c9e230e40c29b9f
Samba Diallo <shabashiki@gmail.com>
2019-12-16 09:49:23 -0800

    only installs bigquery the first run. Now requires table name in secrets as well

--- gcloud_bill.js
+++ gcloud_bill.js
@@ -1,7 +1,6 @@
 // jshint esversion: 9
 var bq;
 const path = "./gcloud_cred.json";
-//var st;
 
 /**
  * @description null
@@ -11,46 +10,45 @@ const path = "./gcloud_cred.json";
  * @return {Promise<SlackBodyType>} Slack response body
  */
 async function _command(params, commandText, secrets = {}) {
-    if (!secrets || !secrets.gcloud_cred_json || !secrets.gcloud_cred_key) {
-        return { body: { text: "You must create secrets for GCloud Billing service to use this command" } };
+    if (!secrets || !secrets.gcloud_cred_json || !secrets.gcloud_cred_key || !secrets.billing_table_name) {
+        return { body: { text: "You must create secrets for GCloud Billing service to use this command.\nSecrets required to run this command are: \ngcloud_cred_json \ngcloud_cred_key \nbilling_table_name" } };
     }
-
+   
     if (!bq) {
-        //Still insalls everytime - need to optimize
+      	//Only installs on the first run after an edit
         console.log(await run("npm install --save @google-cloud/bigquery"));
-        const { BigQuery } = require('@google-cloud/bigquery');
-        bq = BigQuery;
+        bq = require('@google-cloud/bigquery');
     }
 
     let google_app_cred = JSON.parse(secrets.gcloud_cred_json);
     google_app_cred.private_key = JSON.parse(secrets.gcloud_cred_key);
-
+  
     setEV(google_app_cred);
-
-    let usage = await query(google_app_cred.project_id);
-    return { response_type: 'in_channel', text: "GCloud Usage in bytes: " + usage[0].usage };
+  
+	let usage = await query(google_app_cred.project_id, secrets.billing_table_name);
+  	return { response_type: 'in_channel', text: "GCloud Usage in bytes: " + usage[0].usage };
 }
 function setEV(google_app_cred) {
     const fs = require('fs');
-
+    
     if (!fs.existsSync(path)) {
-        console.log("creating file");
-        fs.writeFileSync(path, JSON.stringify(google_app_cred));
+      	console.log("creating file");
+      	fs.writeFileSync(path, JSON.stringify(google_app_cred));
         console.log(JSON.parse(fs.readFileSync(path)));
     }
     process.env.GOOGLE_APPLICATION_CREDENTIALS = path;
 }
-async function query(projectId) {
-    const bqClient = new bq({ projectId: projectId, keyFilename: path });
-
-    //adds up all from the cost column of this test dataset 
-    const sqlQuery = 'select SUM(usage.amount) as usage from nimbellaquiz.gcp_billing_export_v1_01D01B_5F360B_0F357C';
+async function query(projectId, tableName) {
+    const bqClient = new bq.BigQuery({projectId:projectId, keyFilename: path });
+    
+  //adds up all from the cost column of this test dataset 
+    const sqlQuery = 'select SUM(usage.amount) as usage from ' + tableName ;
 
     // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
     const options = {
-        query: sqlQuery,
-        // Location must match that of the dataset(s) referenced in the query.
-        location: 'US',
+      query: sqlQuery,
+      // Location must match that of the dataset(s) referenced in the query.
+      location: 'US',
     };
     // Run the query
     const [rows] = await bqClient.query(options);
